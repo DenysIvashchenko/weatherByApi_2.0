b461462de09f264682f28f4f00237b4666c2b7a2
DenysIvashchenko <71029756+DenysIvashchenko@users.noreply.github.com>
2021-11-18 01:49:09 +0200

    Update url.js

--- scripts/url.js
+++ scripts/url.js
@@ -1,6 +1,6 @@
 const param = {
     "url": "https://api.openweathermap.org/data/2.5/",
-    "appid": "934b3a451ef4d40005c63174fce28dc2"
+    "appid": ""
 };
 const cities = [
     { "Kyiv,ua": "Kyiv" },
